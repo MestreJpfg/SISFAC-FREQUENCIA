c6504b23749a512b2478932f00443844a6e4a2af
MestreJp <mestrejpfg@gmail.com>
2025-10-23 18:33:43 +0000

    Try fixing this error: `Runtime Error: Error: Failed to fetch. Error sou

--- src/app/import/page.tsx
+++ src/app/import/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileUp } from "lucide-react";
 import { ImportForm } from "./ImportForm";
