2ab4a368e5b40eec5e94837094d1a1adc7f6248c
MestreJp <mestrejpfg@gmail.com>
2025-10-23 18:38:33 +0000

    Try fixing this error: `Runtime Error: Error: An unexpected response was

--- src/app/import/page.tsx
+++ src/app/import/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileUp } from "lucide-react";
 import { ImportForm } from "./ImportForm";
