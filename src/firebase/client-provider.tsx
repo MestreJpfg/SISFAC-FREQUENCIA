71985ecda83658e9ee2418187a380b69f55f0870
MestreJp <mestrejpfg@gmail.com>
2025-10-24 12:53:55 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Missing

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,9 +1,10 @@
+
 'use client';
 
 import React, { useMemo, type ReactNode, useEffect, useState } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { initializeFirebase } from '@/firebase';
-import { signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import { signInAnonymously, onAuthStateChanged, User } from 'firebase/auth';
 import { Loader2 } from 'lucide-react';
 
 interface FirebaseClientProviderProps {
@@ -18,22 +19,20 @@ export function FirebaseClientProvider({ children }: FirebaseClientProviderProps
   }, []);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(firebaseServices.auth, async (user) => {
+    const unsubscribe = onAuthStateChanged(firebaseServices.auth, (user) => {
       if (user) {
-        // User is signed in.
+        // User is signed in and ready.
         setIsReady(true);
       } else {
-        // User is signed out. Try to sign in anonymously.
-        try {
-          await signInAnonymously(firebaseServices.auth);
-          // The onAuthStateChanged listener will be called again with the new user,
-          // at which point isReady will be set to true.
-        } catch (error) {
+        // No user is signed in, attempt anonymous sign-in.
+        signInAnonymously(firebaseServices.auth).catch((error) => {
           console.error("Anonymous sign-in failed:", error);
-          // If sign-in fails, we might still want to render the app,
-          // but parts that require auth will fail.
+          // Even if sign-in fails, we'll mark as ready to not block the UI forever.
+          // Parts of the app requiring auth will fail, but at least the app loads.
           setIsReady(true);
-        }
+        });
+        // The onAuthStateChanged listener will be called again once the anonymous
+        // user is signed in, at which point isReady will be set to true.
       }
     });
 
